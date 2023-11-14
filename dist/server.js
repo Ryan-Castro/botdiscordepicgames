a6cfc56b82e3fa2f261f26505cd588ac6ede1eda
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-11-14 18:31:49 -0300

    update

---
+++ dist/server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// src/server.ts
+var import_discord = require("discord.js");
+var import_config = require("dotenv/config");
+var client = new import_discord.Client({ intents: [import_discord.GatewayIntentBits.Guilds] });
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand())
+    return;
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+  if (interaction.commandName === "teste") {
+    await interaction.reply("Teste!!!");
+  }
+  await interaction.reply("Teste!!!");
+});
+client.login(process.env.TOKEN);
